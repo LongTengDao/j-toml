b2da1ce6359eabd9f50b2c28a3eccf9eb9409883
龙腾道 <LongTengDao@LongTengDao.com>
2018-12-18 01:24:25 +0800

    Update global.js

--- src/global.js
+++ src/global.js
@@ -1,2 +1,10 @@
-export const { WeakSet, WeakMap, SyntaxError, RangeError, TypeError, Error, BigInt, Date, parseInt, Infinity, NaN, String: { fromCodePoint }, Number: { isFinite, isSafeInteger }, Object: { create, getOwnPropertyNames, defineProperty }, Reflect: { getPrototypeOf }, Array, Symbol: { for: Symbol_for }, Map, RegExp, JSON: { stringify } } = global;
+export const { WeakSet, WeakMap, SyntaxError, RangeError, TypeError, Error, BigInt, Date, parseInt, Infinity, NaN, Array, Map, RegExp,
+	String: { fromCodePoint },
+	Number: { isFinite, isSafeInteger },
+	Object: { create, getOwnPropertyNames, defineProperty },
+	Reflect: { getPrototypeOf },
+	Symbol: { for: Symbol_for },
+	JSON: { stringify },
+} = global;
+
 export const { isArray } = Array;
