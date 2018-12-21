30d52c5f1507257ed847ef9034d70d7d58f7806e
龙腾道 <LongTengDao@LongTengDao.com>
2018-12-22 02:09:55 +0800

    v0.5.29

--- dist/NPM/index.js
+++ dist/NPM/index.js
@@ -1,6 +1,6 @@
 ﻿'use strict';
 
-const version = '0.5.28';
+const version = '0.5.29';
 
 const { WeakSet, WeakMap: WeakMap$1, SyntaxError, RangeError, TypeError, Error, BigInt, Date, parseInt, Infinity, NaN, Array, Map, RegExp,
 	String: { fromCodePoint },
