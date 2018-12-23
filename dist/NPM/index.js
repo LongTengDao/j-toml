fc3abef2b4b3c40f029b1499ff391ccf73d731fe
龙腾道 <LongTengDao@LongTengDao.com>
2018-12-24 07:32:14 +0800

    ...

--- dist/NPM/index.js
+++ dist/NPM/index.js
@@ -15,8 +15,8 @@ const { isArray } = Array;
 
 /*!
  * 模块名称：@ltd/j-orderify
- * 模块功能：返回一个可以保证给定对象的自有属性按照此后添加的顺序排列的 proxy，即便键名是 symbol，或整数式 string。
-   　　　　　Return a proxy for the given object, which can guarantee the own keys are in setting order, even if the key name is a symbol or an integer-form string.
+ * 模块功能：返回一个能保证给定对象的属性按此后添加顺序排列的 proxy，即使键名是 symbol，或整数 string。
+   　　　　　Return a proxy for given object, which can guarantee own keys are in setting order, even if the key name is symbol or int string.
  * 模块版本：1.0.0
  * 许可条款：LGPL-3.0
  * 所属作者：龙腾道 <LongTengDao@LongTengDao.com> (www.LongTengDao.com)
