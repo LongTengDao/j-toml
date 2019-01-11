125c968a5d3feddc949a2a37a258c9b76478f5fc
龙腾道 <LongTengDao@LongTengDao.com>
2019-01-11 16:35:49 +0800

    ...

--- src/RE.js
+++ src/RE.js
@@ -13,14 +13,14 @@ export const FLOAT_NOT_INTEGER = /[.eE]/;
 
 const Date = /(?:0[1-9]|[1-9]\d)\d\d-(?:0[1-9]|1[012])-(?:0[1-9]|[12]\d|3[01])/;
 const Hour = /[01]\d|2[0-3]/;
-const Minuteorsecond = /[0-5]\d/;
-const Time = /(?:<Hour>):<Minuteorsecond>:<Minuteorsecond>(?:\.\d+)?/;
+const MinuteOrSecond = /[0-5]\d/;
+const Time = /(?:<Hour>):<MinuteOrSecond>:<MinuteOrSecond>(?:\.\d+)?/;
 
-export const OFFSET_DATE_TIME = /^<Date>([T ])<Time>(Z|[+-](?:<Hour>):<Minuteorsecond>)$/;
+export const OFFSET_DATE_TIME = /^<Date>([T ])<Time>(Z|[+-](?:<Hour>):<MinuteOrSecond>)$/;
 export const LOCAL_DATE_TIME = /^<Date>([T ])<Time>$/;
 export const LOCAL_DATE = /^<Date>$/;
 export const LOCAL_TIME = /^<Time>$/;
-export const TIMEZONE_OFFSET = /^([+-])(<Hour>):(<Minuteorsecond>)$/;
+export const TIMEZONE_OFFSET = /^([+-])(<Hour>):(<MinuteOrSecond>)$/;
 
 /* parser */
 
@@ -29,19 +29,19 @@ const Whitespace = /[ \t]/;
 export const BOM = /^\uFEFF/;
 export const EOL = /\r?\n/;
 
-const Basicstringcontent = /(?:[^\\"\x00-\x09\x0B-\x1F\x7F]+|\\(?:[btnfr"\\]|u[0-9A-Fa-f]{4}|U[0-9A-Fa-f]{8}))*/;
-const Literalstringcontent = /[^'\x00-\x08\x0B-\x1F\x7F]*/;
+const BasicStringContent = /(?:[^\\"\x00-\x09\x0B-\x1F\x7F]+|\\(?:[btnfr"\\]|u[0-9A-Fa-f]{4}|U[0-9A-Fa-f]{8}))*/;
+const LiteralStringContent = /[^'\x00-\x08\x0B-\x1F\x7F]*/;
 
 export const PRE_WHITESPACE = /^<Whitespace>*/;
-export const TABLE_DEFINITION = /^\[(\[?)<Whitespace>*((?:[\w-]+|"<Basicstringcontent>"|'<Literalstringcontent>')(?:<Whitespace>*\.<Whitespace>*(?:[\w-]+|"<Basicstringcontent>"|'<Literalstringcontent>'))*)<Whitespace>*](]?)<Whitespace>*(?:#[^]*)?$/;
-export const KEY_VALUE_PAIR = /^((?:[\w-]+|"<Basicstringcontent>"|'<Literalstringcontent>')(?:<Whitespace>*\.<Whitespace>*(?:[\w-]+|"<Basicstringcontent>"|'<Literalstringcontent>'))*)<Whitespace>*=<Whitespace>*(!!([\w-]*)<Whitespace>+)?([^ \t#][^]*)$/;
+export const TABLE_DEFINITION = /^\[(\[?)<Whitespace>*((?:[\w-]+|"<BasicStringContent>"|'<LiteralStringContent>')(?:<Whitespace>*\.<Whitespace>*(?:[\w-]+|"<BasicStringContent>"|'<LiteralStringContent>'))*)<Whitespace>*](]?)<Whitespace>*(?:#[^]*)?$/;
+export const KEY_VALUE_PAIR = /^((?:[\w-]+|"<BasicStringContent>"|'<LiteralStringContent>')(?:<Whitespace>*\.<Whitespace>*(?:[\w-]+|"<BasicStringContent>"|'<LiteralStringContent>'))*)<Whitespace>*=<Whitespace>*(!!([\w-]*)<Whitespace>+)?([^ \t#][^]*)$/;
 export const KEYS = /[\w-]+|"(?:[^\\"]+|\\[^])*"|'[^']*'/g;
 export const VALUE_REST = /^((?:\d\d\d\d-\d\d-\d\d \d)?[\w\-+.:]+)<Whitespace>*([^]*)$/;
-export const LITERAL_STRING = /^'(<Literalstringcontent>)'<Whitespace>*([^]*)/;
+export const LITERAL_STRING = /^'(<LiteralStringContent>)'<Whitespace>*([^]*)/;
 export const MULTI_LINE_LITERAL_STRING_LONE = /^'''([^]*?)'''<Whitespace>*([^]*)/;
 export const MULTI_LINE_LITERAL_STRING_REST = /^([^]*?)'''<Whitespace>*([^]*)/;
 export const CONTROL_CHARACTER_EXCLUDE_TAB = /[\x00-\x08\x0B-\x1F\x7F]/;
-export const BASIC_STRING = /^"(<Basicstringcontent>)"<Whitespace>*([^]*)/;
+export const BASIC_STRING = /^"(<BasicStringContent>)"<Whitespace>*([^]*)/;
 export const MULTI_LINE_BASIC_STRING_LONE = /^"""((?:[^\\]+|\\[^])*?)"""<Whitespace>*([^]*)/;
 export const MULTI_LINE_BASIC_STRING_REST = /^((?:[^\\]+|\\[^])*?)"""<Whitespace>*([^]*)/;
 export const ESCAPED_EXCLUDE_CONTROL_CHARACTER = /^(?:[^\\\x00-\x09\x0B-\x1F\x7F]+|\\(?:[btnfr"\\ \n]|u[0-9A-Fa-f]{4}|U[0-9A-Fa-f]{8}))*$/;
@@ -51,12 +51,12 @@ export const SYM_WHITESPACE = /^[^]<Whitespace>*/;
 export const _VALUE_PAIR = /^!!([\w-]*)<Whitespace>+([^ \t#][^]*)$/;
 
 const String_ = /'[^']*'|"(?:[^\\"]+|\\[^])*"/;
-const Keyvaluepairs = /(?:<String_>)<Whitespace>*=<Whitespace>*(?:<String_>)<Whitespace>*(?:,<Whitespace>*(?:<String_>)<Whitespace>*=<Whitespace>*(?:<String_>)<Whitespace>*)*/;
-const Nonemptyobject = /{<Whitespace>*<Keyvaluepairs>}/;
-const Object = /{<Whitespace>*}|<Nonemptyobject>/;
-const Stringorarray = /<String_>|<Nonemptyobject>|\[<Whitespace>+(?:<String_>)<Whitespace>*(?:,<Whitespace>*(?:<Object>)<Whitespace>*)+]/;
-const Regexpcontent = /(?:[^\\[/]+|\[(?:[^\\\]]+|\\[^])*]|\\[^])+/;
-const Rule = /\(<Whitespace>*(?:\/<Regexpcontent>\/[a-z]*<Whitespace>*=<Whitespace>*(?:<Stringorarray>)<Whitespace>*|<Keyvaluepairs>)\)/;
+const KeyValuePairs = /(?:<String_>)<Whitespace>*=<Whitespace>*(?:<String_>)<Whitespace>*(?:,<Whitespace>*(?:<String_>)<Whitespace>*=<Whitespace>*(?:<String_>)<Whitespace>*)*/;
+const NonEmptyObject = /{<Whitespace>*<KeyValuePairs>}/;
+const Object = /{<Whitespace>*}|<NonEmptyObject>/;
+const StringOrArray = /<String_>|<NonEmptyObject>|\[<Whitespace>+(?:<String_>)<Whitespace>*(?:,<Whitespace>*(?:<Object>)<Whitespace>*)+]/;
+const RegExpContent = /(?:[^\\[/]+|\[(?:[^\\\]]+|\\[^])*]|\\[^])+/;
+const Rule = /\(<Whitespace>*(?:\/<RegExpContent>\/[a-z]*<Whitespace>*=<Whitespace>*(?:<StringOrArray>)<Whitespace>*|<KeyValuePairs>)\)/;
 
 export const SUB = /<Object>/g;
 export const DELIMITER_CHECK = /[^`]/;
@@ -64,6 +64,6 @@ export const INTERPOLATION = /<Rule>/g;
 export const INTERPOLATIONS = /^(?:<Rule><Whitespace>*)*<Whitespace>*([^]*)$/;
 export const INTERPOLATION_TOKEN = /<String_>/g;
 export const REGEXP_MODE = /^\(<Whitespace>*\//;
-export const PATTERN_FLAGS_REPLACER = /\/(<Regexpcontent>)\/([a-z]*)<Whitespace>*=<Whitespace>*(<Stringorarray>)/;
+export const PATTERN_FLAGS_REPLACER = /\/(<RegExpContent>)\/([a-z]*)<Whitespace>*=<Whitespace>*(<StringOrArray>)/;
 export const WHOLE_AND_SUBS = /(<String_>)<Whitespace>*([^]*)/;
 export const DOLLAR = /\$(?:[1-9]\d?|\$)/g;
