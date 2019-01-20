16738275cbe0d0256194b1ef1beb823d5011f8c7
龙腾道 <LongTengDao@LongTengDao.com>
2019-01-20 21:58:01 +0800

    v0.5.39

--- src/RE.js
+++ src/RE.js
@@ -29,13 +29,13 @@ const Whitespace = /[ \t]/;
 export const BOM = /^\uFEFF/;
 export const EOL = /\r?\n/;
 
-const BasicStringContent = /(?:[^\\"\x00-\x09\x0B-\x1F\x7F]|\\(?:[btnfr"\\]|u[0-9A-Fa-f]{4}|U[0-9A-Fa-f]{8}))*/;
+const BasicStringContent = /(?:[^\\"\x00-\x09\x0B-\x1F\x7F]+|\\(?:[btnfr"\\]|u[0-9A-Fa-f]{4}|U[0-9A-Fa-f]{8}))*/;
 const LiteralStringContent = /[^'\x00-\x08\x0B-\x1F\x7F]*/;
 
 export const PRE_WHITESPACE = /^<Whitespace>*/;
 export const TABLE_DEFINITION = /^\[(\[?)<Whitespace>*((?:[\w-]+|"<BasicStringContent>"|'<LiteralStringContent>')(?:<Whitespace>*\.<Whitespace>*(?:[\w-]+|"<BasicStringContent>"|'<LiteralStringContent>'))*)<Whitespace>*](]?)<Whitespace>*(?:#[^]*)?$/;
 export const KEY_VALUE_PAIR = /^((?:[\w-]+|"<BasicStringContent>"|'<LiteralStringContent>')(?:<Whitespace>*\.<Whitespace>*(?:[\w-]+|"<BasicStringContent>"|'<LiteralStringContent>'))*)<Whitespace>*=<Whitespace>*(!!([\w-]*)<Whitespace>+)?([^ \t#][^]*)$/;
-export const KEYS = /[\w-]+|"(?:[^\\"]|\\[^])*"|'[^']*'/g;
+export const KEYS = /[\w-]+|"(?:[^\\"]+|\\[^])*"|'[^']*'/g;
 export const VALUE_REST = /^((?:\d\d\d\d-\d\d-\d\d \d)?[\w\-+.:]+)<Whitespace>*([^]*)$/;
 export const LITERAL_STRING = /^'(<LiteralStringContent>)'<Whitespace>*([^]*)/;
 export const MULTI_LINE_LITERAL_STRING_LONE = /^'''([^]*?)'''<Whitespace>*([^]*)/;
@@ -44,18 +44,18 @@ export const CONTROL_CHARACTER_EXCLUDE_TAB = /[\x00-\x08\x0B-\x1F\x7F]/;
 export const BASIC_STRING = /^"(<BasicStringContent>)"<Whitespace>*([^]*)/;
 export const MULTI_LINE_BASIC_STRING_LONE = /^"""((?:[^\\]|\\[^])*?)"""<Whitespace>*([^]*)/;
 export const MULTI_LINE_BASIC_STRING_REST = /^((?:[^\\]|\\[^])*?)"""<Whitespace>*([^]*)/;
-export const ESCAPED_EXCLUDE_CONTROL_CHARACTER = /^(?:[^\\\x00-\x09\x0B-\x1F\x7F]|\\(?:[btnfr"\\ \n]|u[0-9A-Fa-f]{4}|U[0-9A-Fa-f]{8}))*$/;
+export const ESCAPED_EXCLUDE_CONTROL_CHARACTER = /^(?:[^\\\x00-\x09\x0B-\x1F\x7F]+|\\(?:[btnfr"\\ \n]|u[0-9A-Fa-f]{4}|U[0-9A-Fa-f]{8}))*$/;
 export const ESCAPED_IN_MULTI_LINE = /\n|\\(?:([ \n]+)|([\\"])|([btnfr])|u(.{4})|U(.{4})(.{4}))/g;
 export const SYM_WHITESPACE = /^[^]<Whitespace>*/;
 
 export const _VALUE_PAIR = /^!!([\w-]*)<Whitespace>+([^ \t#][^]*)$/;
 
-const String_ = /'[^']*'|"(?:[^\\"]|\\[^])*"/;
+const String_ = /'[^']*'|"(?:[^\\"]+|\\[^])*"/;
 const KeyValuePairs = /(?:<String_>)<Whitespace>*=<Whitespace>*(?:<String_>)<Whitespace>*(?:,<Whitespace>*(?:<String_>)<Whitespace>*=<Whitespace>*(?:<String_>)<Whitespace>*)*/;
 const NonEmptyObject = /{<Whitespace>*<KeyValuePairs>}/;
 const Object = /{<Whitespace>*}|<NonEmptyObject>/;
 const StringOrArray = /<String_>|<NonEmptyObject>|\[<Whitespace>+(?:<String_>)<Whitespace>*(?:,<Whitespace>*(?:<Object>)<Whitespace>*)+]/;
-const RegExpContent = /(?:[^\\[/]|\[(?:[^\\\]]|\\[^])*]|\\[^])+/;
+const RegExpContent = /(?:[^\\[/]+|\[(?:[^\\\]]+|\\[^])*]|\\[^])+/;
 const Rule = /\(<Whitespace>*(?:\/<RegExpContent>\/[a-z]*<Whitespace>*=<Whitespace>*(?:<StringOrArray>)<Whitespace>*|<KeyValuePairs>)\)/;
 
 export const SUB = /<Object>/g;
