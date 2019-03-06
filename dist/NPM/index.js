a075c9a17053d3af78c6e8a72df980bf97f8dc74
龙腾道 <LongTengDao@LongTengDao.com>
2019-03-06 17:22:07 +0800

    v0.5.57

--- dist/NPM/index.js
+++ dist/NPM/index.js
@@ -1,6 +1,6 @@
 ﻿'use strict';
 
-const version = '0.5.56';
+const version = '0.5.57';
 
 const isArray = Array.isArray;
 
@@ -44,7 +44,7 @@ function throws(error) {
  * 模块名称：@ltd/j-orderify
  * 模块功能：返回一个能保证给定对象的属性按此后添加顺序排列的 proxy，即使键名是 symbol，或整数 string。
    　　　　　Return a proxy for given object, which can guarantee own keys are in setting order, even if the key name is symbol or int string.
- * 模块版本：2.3.1
+ * 模块版本：2.4.0
  * 许可条款：LGPL-3.0
  * 所属作者：龙腾道 <LongTengDao@LongTengDao.com> (www.LongTengDao.com)
  * 问题反馈：https://GitHub.com/LongTengDao/j-orderify/issues
