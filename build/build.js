3c49d461eaac4370b0ec28e915f33ba1ca565cdd
龙腾道 <LongTengDao@LongTengDao.com>
2019-03-10 02:29:23 +0800

    docs

--- build/build.js
+++ build/build.js
@@ -18,8 +18,16 @@ require('../test/test.js')(async ({ build, get, map, ful }) => {
 	});
 	
 	await map(
-		['docs/README.md', string => string.replace(/(\n```+)[^`\r\n]+/g, '$1').replace(/(\n\d\. {2})#+ +([^\r\n]*)/g, '$1**$2**'), 'dist/NPM/README.md'],
+		['docs/English/README.md', ReadMe, 'dist/NPM/README.md'],
 		['src/d.ts', 'dist/TSD/j-toml.d.ts'],
 	);
 	
 });
+
+function ReadMe (English) {
+	const i18n = ['English', '简体中文'];
+	const eol = /\r?\n/.exec(English)[0];
+	return eol+
+		i18n.map(lang => `[${lang}](https://GitHub.com/LongTengDao/j-toml/tree/master/docs/${lang}/)`).join(' | ')+eol+
+		English.replace(/(\n```+)[^`\r\n]+/g, '$1').replace(/(\n\d\. {2})#+ +([^\r\n]*)/g, '$1**$2**');
+}
