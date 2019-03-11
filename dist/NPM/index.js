8fa33b2d7b294347ef0f469c2c3bc7cafe4ac43f
龙腾道 <LongTengDao@LongTengDao.com>
2019-03-12 05:10:56 +0800

    v0.5.60

--- dist/NPM/index.js
+++ dist/NPM/index.js
@@ -1,6 +1,6 @@
 ﻿'use strict';
 
-const version = '0.5.59';
+const version = '0.5.60';
 
 const isArray = Array.isArray;
 
@@ -44,12 +44,14 @@ function throws(error) {
  * 模块名称：@ltd/j-orderify
  * 模块功能：返回一个能保证给定对象的属性按此后添加顺序排列的 proxy，即使键名是 symbol，或整数 string。
    　　　　　Return a proxy for given object, which can guarantee own keys are in setting order, even if the key name is symbol or int string.
- * 模块版本：2.4.0
+ * 模块版本：2.5.0
  * 许可条款：LGPL-3.0
  * 所属作者：龙腾道 <LongTengDao@LongTengDao.com> (www.LongTengDao.com)
  * 问题反馈：https://GitHub.com/LongTengDao/j-orderify/issues
  * 项目主页：https://GitHub.com/LongTengDao/j-orderify/
  */
+
+const create = Object.create;
 
 const defineProperty = Reflect.defineProperty;
 
@@ -58,7 +60,7 @@ const deleteProperty = Reflect.deleteProperty;
 const ownKeys = Reflect.ownKeys;
 
 const ownKeysKeepers = new WeakMap;
-const handlers = Object.create(null, {
+const handlers = create(null, {
     defineProperty: {
         value(target, key, descriptor) {
             if (defineProperty(target, key, descriptor)) {
@@ -89,8 +91,6 @@ const orderify = (object) => {
 };
 
 /*¡ @ltd/j-orderify */
-
-const create = Object.create;
 
 function Table() { }
 const OrderedTable = function Table() { return orderify(this); };
