6ca3da9771177a75b77841ba13bb932a8441bdd2
龙腾道 <LongTengDao@LongTengDao.com>
2019-03-25 08:21:39 +0800

    v0.5.81

--- dist/NPM/index.js
+++ dist/NPM/index.js
@@ -1,6 +1,6 @@
 ﻿'use strict';
 
-const version = '0.5.80';
+const version = '0.5.81';
 
 const isBuffer = Buffer.isBuffer;
 
@@ -73,7 +73,7 @@ const isSafeInteger = Number.isSafeInteger;
  * 模块名称：@ltd/j-orderify
  * 模块功能：返回一个能保证给定对象的属性按此后添加顺序排列的 proxy，即使键名是 symbol，或整数 string。
    　　　　　Return a proxy for given object, which can guarantee own keys are in setting order, even if the key name is symbol or int string.
- * 模块版本：2.6.0
+ * 模块版本：2.7.1
  * 许可条款：LGPL-3.0
  * 所属作者：龙腾道 <LongTengDao@LongTengDao.com> (www.LongTengDao.com)
  * 问题反馈：https://GitHub.com/LongTengDao/j-orderify/issues
@@ -295,7 +295,7 @@ function use(specificationVersion, multiLineJoiner, useBigInt, xOptions) {
  * 模块名称：@ltd/j-regexp
  * 模块功能：可读性更好的正则表达式创建方式。
    　　　　　More readable way for creating RegExp.
- * 模块版本：1.0.0
+ * 模块版本：1.1.0
  * 许可条款：LGPL-3.0
  * 所属作者：龙腾道 <LongTengDao@LongTengDao.com> (www.LongTengDao.com)
  * 问题反馈：https://GitHub.com/LongTengDao/j-regexp/issues
